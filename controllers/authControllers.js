94ccecf83418539cf53cc9ced17b3bffa23b83e5
Jim Ballard <AntlerHorse@gmail.com>
2026-03-28 06:49:40 -0400

    end of section 13 with everything working

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,23 +1,36 @@
 const adminModel = require('../models/adminModel')
-const { responseReture } = require('../Utilities/response')
+const { responseReturn } = require('../utilities/response')
 const bcrypt = require('bcrypt')
+const { createToken } = require('../utilities/tokenCreate')
 
 class authControllers{
     admin_login = async(req,res) => {
-        const {email,password} = req.body
+        const {email, password} = req.body
    
         try {
-            console.log('email = ' + email)
             const admin = await adminModel.findOne({email}).select('+password')
             //console.log(admin)
             if(admin) {
                 const match = await bcrypt.compare(password, admin.password)
-                console.log(match)
+                //console.log(match)
+                if(match) {
+                     const token = await createToken({
+                        id: admin.id,
+                        role: admin.role
+                     })
+                     res.cookie('accessToken', token, {
+                        expires: new Date(Date.now() + 7*24*60*60*1000)
+                     })
+                     responseReturn(res, 200, {token, message: "Login Success"})
+                } else {
+                    responseReturn(res, 404, {error: "Password is WRONG!!!"})
+                }
+
             } else {
-                responseReture(res,500,{error: "Email not found"})
+                responseReturn(res, 500, {error: "Email not found"})
             }
         } catch (error) {
-            responseReture(res,500,{error: error.message})
+            responseReturn(res, 500, {error: error.message})
         }
     }
 }
