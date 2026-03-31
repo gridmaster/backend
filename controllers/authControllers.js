5bae71b1ab777fe2ed189c9113943d5cae32ffa5
Jim Ballard <AntlerHorse@gmail.com>
2026-03-30 23:58:37 -0400

    toast popups working

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -4,35 +4,42 @@ const bcrypt = require('bcrypt')
 const { createToken } = require('../utilities/tokenCreate')
 
 class authControllers{
-    admin_login = async(req,res) => {
-        const {email, password} = req.body
    
+    admin_login = async(req,res) => {
+        const {email,password} = req.body
         try {
             const admin = await adminModel.findOne({email}).select('+password')
-            //console.log(admin)
-            if(admin) {
+            // console.log(admin)
+            if (admin) {
                 const match = await bcrypt.compare(password, admin.password)
-                //console.log(match)
-                if(match) {
-                     const token = await createToken({
-                        id: admin.id,
-                        role: admin.role
-                     })
-                     res.cookie('accessToken', token, {
-                        expires: new Date(Date.now() + 7*24*60*60*1000)
-                     })
-                     responseReturn(res, 200, {token, message: "Login Success"})
+                // console.log(match)
+                if (match) {
+                    const token = await createToken({
+                        id : admin.id,
+                        role : admin.role
+                    })
+                    res.cookie('accessToken', token, {
+                        httpOnly: true,
+                        secure: true,
+                        sameSite: 'none',
+                        maxAge: 7 * 24 * 60 * 60 * 1000
+                    }) 
+                    responseReturn(res,200,{token, message: "Login Success"})
                 } else {
-                    responseReturn(res, 404, {error: "Password is WRONG!!!"})
+                    responseReturn(res,404,{error: "Password Wrong"})
                 }
-
+                 
             } else {
-                responseReturn(res, 500, {error: "Email not found"})
+                responseReturn(res,404,{error: "Email not Found"})
             }
+            
         } catch (error) {
-            responseReturn(res, 500, {error: error.message})
+            responseReturn(res,500,{error: error.message})
         }
+ 
     }
+    // End Method 
+
 }
 
 module.exports = new authControllers()
